3e7c39c09c2ac6bc5be2aed12db3b8edcbe23e72
tacrossman <thomas@rehabstudio.com>
2013-06-11 14:52:09 +0100

    map and masonry/isotope

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,65 +1,47 @@
-function initialize() {
-  var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
-  var myLatlng2 = new google.maps.LatLng(-23.363882,120.044922);
+var map;
 
-  var mapOptions = {
-    zoom: 4,
-    center: myLatlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
+jQuery(function($) {
+    $(document).ready(function() {
+        var latlng = new google.maps.LatLng(-24.397, 130.644);
+        var myOptions = {
+            zoom: 4,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+        google.maps.event.trigger(map, 'resize');
 
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        $('a[href="#profile"]').on('shown', function(e) {
+            google.maps.event.trigger(map, 'resize');
+        });
+        $("#map_canvas").css("width", 1100).css("height", 500);
+    });
+});
 
-  var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Mrs Smith</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Mrs Smith</b>, teaches English.</p>'+
-      '<p><a href="#">View Profile</a>'+
-      '</div>';
 
-  var contentString2 = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Mrs Jones</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Mrs Jones</b>, teaches Science.</p>'+
-      '<p><a href="#">View Profile</a>'+
-      '</div>';
+    $(document).ready(function(){
+    var $container = $('#content');
+$container.isotope({
+  filter: '*',
+  animationOptions: {
+     duration: 750,
+     easing: 'linear',
+     queue: false,
+   }
+});
 
-  var infowindow = new google.maps.InfoWindow({
-      content: contentString,
-      maxWidth: 400
+$('#nav a').click(function(){
+  var selector = $(this).attr('data-filter');
+    $container.isotope({ 
+  filter: selector,
+  animationOptions: {
+     duration: 750,
+     easing: 'linear',
+     queue: false,
+   
+   }
   });
+  return false;
+});
 
-  var infowindow2 = new google.maps.InfoWindow({
-      content: contentString2,
-      maxWidth: 400
-  });
-
-  var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      animation: google.maps.Animation.DROP,
-      title: 'Mrs Smith - Uluru School'
-  });
-
-  var marker2 = new google.maps.Marker({
-      position: myLatlng2,
-      map: map,
-      animation: google.maps.Animation.DROP,      
-      title: 'Mrs Jones - Walalala School'
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map,marker);
-  });
-  
-  google.maps.event.addListener(marker2, 'click', function() {
-    infowindow2.open(map,marker2);
-  });
-
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+});
